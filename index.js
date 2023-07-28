bdd0126c968578a4f6ae4a5b27b929682f46dd49
ABU TAHER <abutaher267@gmail.com>
2023-07-28 16:46:02 +0600

    fjsakj

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { createServer } from "http";
+import express from "express";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { WebSocketServer } from "ws";
+import { useServer } from "graphql-ws/lib/use/ws";
+import bodyParser from "body-parser";
+import cors from "cors";
 import resolvers from "./resolvers.js";
 import typeDefs from "./typeDefs.js";
-import { makeExecutableSchema } from "@graphql-tools/schema";
 import {
     constraintDirective,
     constraintDirectiveTypeDefs,
 } from "graphql-constraint-directive";
 import respErrsKeyValues from "./helper/respErrsKeyValues.js";
+// Create the schema, which will be used separately by ApolloServer and
+// the WebSocket server
 
 let schema = makeExecutableSchema({
     typeDefs: [constraintDirectiveTypeDefs, typeDefs],
     resolvers,
-
 });
 schema = constraintDirective()(schema);
 
+// Create an Express app and HTTP server; we will attach both the WebSocket
+// server and the ApolloServer to this HTTP server.
+const app = express();
+const httpServer = createServer(app);
 
-// Hand in the schema we just created and have the
-// WebSocketServer start listening.
+// Create our WebSocket server using the HTTP server we just set up.
+const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/",
+});
+// Save the returned server's info so we can shutdown this server later
+const serverCleanup = useServer({ schema }, wsServer);
+
+// Set up ApolloServer.
 const server = new ApolloServer({
     schema,
     plugins: [
+        // Proper shutdown for the HTTP server.
+        ApolloServerPluginDrainHttpServer({ httpServer }),
         {
             async requestDidStart() {
                 return {
@@ -35,18 +56,26 @@ const server = new ApolloServer({
                 };
             },
         },
+        // Proper shutdown for the WebSocket server.
+        {
+            async serverWillStart() {
+                return {
+                    async drainServer() {
+                        await serverCleanup.dispose();
+                    },
+                };
+            },
+        },
     ],
-
 });
-(async () => {
-    const { url } = await startStandaloneServer(server);
-    console.log(`🚀 Server ready at ${url}`);
-})();
 
-// const { url } = await startStandaloneServer(server, {
-//     context: () => { },
-//     // listen: {
-//     //     port: process.env.PORT || 4000
-//     // }
-// });
-// console.log(`🚀 Server ready at ${url}`);
+(async () => {
+    await server.start();
+
+    app.use("/", cors(), bodyParser.json(), expressMiddleware(server));
+    const PORT = process.env.PORT || 4000;
+    // Now that our HTTP server is fully set up, we can listen to it.
+    httpServer.listen(PORT, () => {
+        console.log(`Server is now running on http://localhost:${PORT}`);
+    });
+})();
